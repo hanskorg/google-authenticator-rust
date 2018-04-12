895012a8112e9e26bf069444076f2af82e10c440
hans <hans@hansk.org>
2018-04-13 03:32:18 +0800

    impl create_secret

--- src/google_authenticator.rs
+++ src/google_authenticator.rs
@@ -1,12 +1,44 @@
 
-struct GoogleAuthenticator{
+use rand;
+pub struct GoogleAuthenticator{
     pub code_len:u8,
+    _base32_lookup_table:Vec<char>
 }
 
 impl GoogleAuthenticator{
 
-    pub fn create_secret(length:u8) -> &str{
-        "";
+    pub fn new(length:u8) -> GoogleAuthenticator{
+        GoogleAuthenticator{
+            code_len:length,
+            _base32_lookup_table: vec![
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', //  7
+                'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', // 15
+                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', // 23
+                'Y', 'Z', '2', '3', '4', '5', '6', '7', // 31
+                '=',
+            ]
+        }
     }
+    pub fn create_secret(&self, length:u8) -> String{
+        let rand_bytes = rand::random::<[u8;32]>();
+        let mut secret = Vec::<char>::new();
+        let mut index: usize;
+        for i in 0 .. length {
+            index = (rand_bytes[i as usize] & 31) as usize;
+            secret.push(self._base32_lookup_table[ index ]);
+        }
+        secret.into_iter().collect()
+    }
+
+    pub fn get_code(&self, secret:&str, times_slice:u32) -> u32{
+        123432
+    }
+
+    pub fn verify_code(&self, code: u32) -> bool{
+        false
+    }
+
+
+
 
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
+extern crate rand;
+
+pub mod google_authenticator;
+pub use google_authenticator::GoogleAuthenticator;
+
 #[cfg(test)]
 mod tests {
+    use google_authenticator::GoogleAuthenticator;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn create_secret() {
+        let secret = GoogleAuthenticator::new(6u8).create_secret(32);
+        println!("{:?}",secret);
+        assert_eq!(secret.len(),32);
     }
 }
