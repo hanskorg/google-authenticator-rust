c4f2dfcdb99f43c1b19649ec4b46fb203e0c352d
hans <hans@hansk.org>
2018-04-13 22:04:08 +0800

    gen code & verify code

--- src/google_authenticator.rs
+++ src/google_authenticator.rs
@@ -1,16 +1,21 @@
 
 use rand;
+use base32;
+use hmacsha1::hmac_sha1;
+use std::mem;
+use std::time::{SystemTime, UNIX_EPOCH};
+
 pub struct GoogleAuthenticator{
-    pub code_len:u8,
-    _base32_lookup_table:Vec<char>
+    code_len:usize,
+    _base32_alphabet:Vec<char>
 }
 
 impl GoogleAuthenticator{
 
-    pub fn new(length:u8) -> GoogleAuthenticator{
+    pub fn new() -> GoogleAuthenticator{
         GoogleAuthenticator{
-            code_len:length,
-            _base32_lookup_table: vec![
+            code_len: 6,
+            _base32_alphabet: vec![
                 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', //  7
                 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', // 15
                 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', // 23
@@ -24,18 +29,59 @@ impl GoogleAuthenticator{
         let mut secret = Vec::<char>::new();
         let mut index: usize;
         for i in 0 .. length {
-            index = (rand_bytes[i as usize] & 31) as usize;
-            secret.push(self._base32_lookup_table[ index ]);
+            index = (rand_bytes[i as usize] & 0x1F) as usize;
+            secret.push(self._base32_alphabet[ index ]);
         }
         secret.into_iter().collect()
     }
 
-    pub fn get_code(&self, secret:&str, times_slice:u32) -> u32{
-        123432
+    pub fn get_code(&self, secret:&str, times_slice:u32) -> String{
+        let mut message: u32 = times_slice;
+        if times_slice == 0 {
+            message = (f64::from_bits(SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()) / 30.0).floor() as u32;
+        }
+        let key  = self._base32_decode(secret);
+        let msg_bytes = unsafe { mem::transmute::<u32,[u8;4]>(message.to_be()) };
+        let mut message_body:Vec<u8> = vec![0;4];
+        for msg_byte in msg_bytes.iter() {
+            message_body.push(*msg_byte);
+        }
+        let hash = hmac_sha1(&key, message_body.as_slice());
+
+        let offset = hash[hash.len() - 1] & 0x0F;
+        let mut truncated_hash:[u8;4] = Default::default();
+        truncated_hash.copy_from_slice(&hash[offset  as usize .. (offset + 4)  as usize]);
+        let mut code:u32 = unsafe { mem::transmute::<[u8;4], u32>(truncated_hash) };
+        if cfg!(target_endian = "big") {
+        } else {
+            code = u32::from_be(code);
+        }
+        code =  code % 1_000_000u32;
+        let mut code_str = code.to_string();
+        for i in 0 .. (self.code_len - code_str.len()) {
+            code_str.insert(i,'0');
+        }
+        code_str
     }
 
-    pub fn verify_code(&self, code: u32) -> bool{
+    pub fn verify_code(&self, secret:&str, code: &str, discrepancy:i32, time_slice:u32) -> bool{
+        let mut curr_time_slice: u32 = time_slice;
+        if time_slice == 0 {
+            curr_time_slice = (f64::from_bits(SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()) / 30.0).floor() as u32;
+        }
+        if code.len() != self.code_len {
+            return false;
+        }
+        for offset in (discrepancy * -1) .. discrepancy   {
+            if code.eq(self.get_code(secret, curr_time_slice ).as_str()) {
+                return true;
+            }
+        }
         false
+    }
+
+    fn _base32_decode(&self, secret:&str) -> Vec<u8>{
+        return base32::decode(base32::Alphabet::RFC4648 { padding: true }, secret).unwrap()
     }
 
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 extern crate rand;
+extern crate base32;
+extern crate hmacsha1;
 
 pub mod google_authenticator;
 pub use google_authenticator::GoogleAuthenticator;
@@ -6,10 +8,26 @@ pub use google_authenticator::GoogleAuthenticator;
 #[cfg(test)]
 mod tests {
     use google_authenticator::GoogleAuthenticator;
-    #[test]
     fn create_secret() {
-        let secret = GoogleAuthenticator::new(6u8).create_secret(32);
-        println!("{:?}",secret);
+        let auth = GoogleAuthenticator::new();
+        let secret = auth.create_secret(32);
+        //auth.get_code(secret.as_str(),0);
+//        println!("{:?}",secret);
         assert_eq!(secret.len(),32);
+    }
+    #[test]
+    fn test_code(){
+        let auth = GoogleAuthenticator::new();
+        let secret = "I3VFM3JKMNDJCDH5BMBEEQAW6KJ6NOE3";
+        //otpauth://totp/test?secret=I3VFM3JKMNDJCDH5BMBEEQAW6KJ6NOE3 1523610659 559389
+        println!("{}",1523610659 / 30);
+        assert_eq!(auth.get_code(secret,  1523610659 / 30), "224124");
+    }
+
+    #[test]
+    fn test_verify_code(){
+        let auth = GoogleAuthenticator::new();
+        let secret = "I3VFM3JKMNDJCDH5BMBEEQAW6KJ6NOE3";
+        assert!(auth.verify_code(secret, "224124", 1, 523610659 / 30));
     }
 }
