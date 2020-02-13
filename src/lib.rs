d9cd61b8037c346aacde409ff8e9db7531e9babf
zjhsdtc <zjhsdtc@gmail.com>
2020-02-13 22:28:08 +0800

    feat: add macros for simlify API exporting

--- src/lib.rs
+++ src/lib.rs
@@ -29,6 +29,8 @@
 //! ```
 //!
 //!
+#[macro_use]
+extern crate lazy_static;
 extern crate rand;
 extern crate base32;
 extern crate hmacsha1;
@@ -40,6 +42,60 @@ extern crate qrcode;
 pub mod google_authenticator;
 
 pub use google_authenticator::{GoogleAuthenticator,GAError};
+
+lazy_static! {
+    pub static ref GA_AUTH: GoogleAuthenticator = GoogleAuthenticator::new();
+}
+
+#[macro_export]
+macro_rules! create_secret {
+    ($length: expr) => {
+        GA_AUTH.create_secret($length)
+    };
+    () => {
+        GA_AUTH.create_secret(32)
+    };
+}
+
+#[macro_export]
+macro_rules! get_code {
+    ($secret: expr, $time_slice: expr) => {
+        GA_AUTH.get_code($secret, time_slice)
+    };
+    ($secret: expr) => {
+        GA_AUTH.get_code($secret, 0)
+    };
+}
+
+#[macro_export]
+macro_rules! verify_code {
+    ($secret: expr, $code: expr, $discrepancy: expr, $time_slice: expr) => {
+        GA_AUTH.verify_code($secret, $code, $discrepancy, $time_slice)
+    };
+    ($secret: expr, $code: expr) => {
+        GA_AUTH.verify_code($secret, $code, 1, 0)
+    };
+}
+
+#[macro_export]
+macro_rules! qr_code_url {
+    ($secret: expr, $name: expr, $title: expr, $width: expr, $height: expr, $level: expr) => {
+        GA_AUTH.qr_code_url($secret, $name, $title, $width, $height, $level)
+    };
+    ($secret: expr, $name: expr, $title: expr) => {
+        GA_AUTH.qr_code_url($secret, $name, $title, 200, 200, 'M')
+    };
+}
+
+#[macro_export]
+macro_rules! qr_code {
+    ($secret: expr, $name: expr, $title: expr, $width: expr, $height: expr, $level: expr) => {
+        GA_AUTH.qr_code($secret, $name, $title, $width, $height, $level)
+    };
+    ($secret: expr, $name: expr, $title: expr) => {
+        GA_AUTH.qr_code($secret, $name, $title, 200, 200, 'M')
+    };
+}
 
 #[cfg(test)]
 mod tests {
@@ -67,5 +123,29 @@ mod tests {
         let secret = "I3VFM3JKMNDJCDH5BMBEEQAW6KJ6NOE3";
         println!("{:?}",auth.qr_code(secret,"qr_code","name",0,0,'H'));
         assert!(auth.verify_code(secret, "224124", 3, 1523610659 / 30).unwrap());
+    }
+}
+
+#[cfg(test)]
+mod macro_tests {
+    use GA_AUTH;
+
+    #[test]
+    fn create_secret() {
+        let secret = create_secret!();
+        assert_eq!(secret.len(), 32);
+    }
+
+    #[test]
+    fn test_code() {
+        let secret = "I3VFM3JKMNDJCDH5BMBEEQAW6KJ6NOE3";
+        assert_eq!(6, get_code!(&secret).unwrap().len());
+    }
+
+    #[test]
+    #[cfg(any(feature = "with-qrcode"))]
+    fn test_verify_code() {
+        let secret = "I3VFM3JKMNDJCDH5BMBEEQAW6KJ6NOE3";
+        assert!(verify_code!(secret, "224124").unwrap());
     }
 }
